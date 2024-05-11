205e9c557f558b7c89c8e6b49f925136952debc5
Ana Carolina <115048267+ana-caroll@users.noreply.github.com>
2024-05-11 18:24:40 -0300

    principal alterada

--- PrincipalTCC.php
+++ PrincipalTCC.php
@@ -1,9 +1,9 @@
-<!DOCTYPE html>
+!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="stylePrincipal.css">
+    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <script src="https://kit.fontawesome.com/8a0118f486.js" crossorigin="anonymous"></script>
     <title>Controle Financeiro</title>
@@ -20,6 +20,9 @@
     </style>
 </head>
 <body class="w3-light-grey">
+    <header>
+        <img src="img/logo.jpeg" alt="Logo" class="w3-image w3-display-topmiddle" width="10%">
+    </header>
     <div class= "barra_lateral">
         <nav class="w3-sidebar w3-bar-block w3-center w3-green" id="sidebar">
             <a href="#inicio" class="w3-bar-item w3-button w3-block n w3-cell w3-hover-light-grey w3-hover-text-green w3-text-light-grey" id="botao_inicio">
@@ -31,7 +34,7 @@
                 <i class="far fa-address-card w3-xxlarge"></i>
                 <p>Informação financeira</p>
             </a>
-            <a href="#objetivo" class="w3-bar-item w3-button w3-block n w3-cell w3-hover-light-grey w3-hover-text-green w3-text-light-grey">
+            <a href="#robjetivo" class="w3-bar-item w3-button w3-block n w3-cell w3-hover-light-grey w3-hover-text-green w3-text-light-grey">
                 <i class="fas fa-balance-scale w3-xxlarge"></i>
                 <p>Objetivo</p>
             </a>
@@ -45,7 +48,7 @@
     <!--Incial -->
     <header class="w3-container w3-padding-32 w3-center " id="inicio">
         <h1>
-            <img src="img/logo2.jpeg" alt="Logo" class="w3-image" width="10%">
+        <img src="img/controleSemFundo.png" alt="Logo" class="w3-image" width="20%">
             </br>
         </h1>
         <br>
@@ -81,101 +84,102 @@
 </div>
 
     <!-- informações financeiras -->
-    <div class="w3-padding-128 w3-content w3-text-grey" id="ifinanceira">
-    <h2 class="w3-text-green">Dados Pessoais</h2>
+    <div class=" w3-content w3-text-grey w3-card" id="ifinanceira">
+    <h2 class="w3-text-green">Informações financeiras</h2>
 
-    <div class="w3-row w3-section">
-        <div class="w3-col" style="width:11%;">
-            <i class="w3-xxlarge far fa-grin-alt"></i>
+        <div class="w3-row w3-section">
+            <div class="w3-col" style="width:11%;" id="icones_dados_pessoais">
+                <i class="w3-xxlarge far fa-grin-alt"></i>
+            </div>
+            <div class="w3-rest">
+                <input class="w3-input w3-border w3-round-large" name="txtNome" type="text" placeholder="Nome" value="">
+            </div>
         </div>
-        <div class="w3-rest">
-            <input class="w3-input w3-border w3-round-large" name="txtNome" type="text" placeholder="Nome" value="">
-        </div>
-    </div>
 
-    <div class="w3-row w3-section">
-        <div class="w3-col" style="width: 11%;">
-            <i class="w3-xxlarge fas fa-briefcase"></i>
+        <div class="w3-row w3-section">
+            <div class="w3-col" style="width: 11%;" id="icones_dados_pessoais">
+                <i class="w3-xxlarge fas fa-briefcase"></i>
+            </div>
+            <div class="w3-rest">
+                <input class="w3-input w3-border w3-round-large" name="txtOcupacao" type="text" placeholder="Ocupação" value="">
+            </div>
         </div>
-        <div class="w3-rest">
-            <input class="w3-input w3-border w3-round-large" name="txtOcupacao" type="text" placeholder="Ocupação" value="">
-        </div>
-    </div>
 
-    <div class="w3-row w3-section">
-        <div class="w3-col" style="width:11%;">
-            <i class="w3-xxlarge fas fa-building"></i>
+        <div class="w3-row w3-section">
+            <div class="w3-col" style="width:11%;" id="icones_dados_pessoais">
+                <i class="w3-xxlarge fas fa-building"></i>
+            </div>
+            <div class="w3-rest">
+                <input class="w3-input w3-border w3-round-large" name="txtRenda" type="text" placeholder="Renda mensal" value="">
+            </div>
         </div>
-        <div class="w3-rest">
-            <input class="w3-input w3-border w3-round-large" name="txtRenda" type="text" placeholder="Renda mensal" value="">
-        </div>
-    </div>
 
-    <div class="w3-row w3-section">
-        <div class="w3-col" style="width:11%;">
-            <i class="w3-xxlarge fas fa-folder-open"></i>
+        <div class="w3-row w3-section">
+            <div class="w3-col" style="width:11%;" id="icones_dados_pessoais">
+                <i class="w3-xxlarge fas fa-folder-open"></i>
+            </div>
+            <div class="w3-rest">
+                <input class="w3-input w3-border w3-round-large" name="txtRendaExtra" type="text" placeholder="Renda Extra" value="">
+            </div>
         </div>
-        <div class="w3-rest">
-            <input class="w3-input w3-border w3-round-large" name="txtRendaExtra" type="text" placeholder="Renda Extra" value="">
-        </div>
-    </div>
 
-    <div class="w3-row w3-section">
-        <div class="w3-col" style="width:11%;">
-            <i class="w3-xxlarge fas fa-car-crash"></i>
+        <div class="w3-row w3-section">
+            <div class="w3-col" style="width:11%;" id="icones_dados_pessoais">
+                <i class="w3-xxlarge fas fa-car-crash"></i>
+            </div>
+            <div class="w3-rest">
+                <input class="w3-input w3-border w3-round-large" name="txtDespesasPrevistas" type="text" placeholder="Despesas Mensais" value="">
+            </div>
         </div>
-        <div class="w3-rest">
-            <input class="w3-input w3-border w3-round-large" name="txtDespesasPrevistas" type="text" placeholder="Despesas Mensais" value="">
-        </div>
-    </div>
 
-    <div class="w3-row w3-section">
-        <div class="w3-col" style="width:11%;">
-            <i class="w3-xxlarge fas fa-tools"></i>
+        <div class="w3-row w3-section">
+            <div class="w3-col" style="width:11%;" id="icones_dados_pessoais">
+                <i class="w3-xxlarge fas fa-tools"></i>
+            </div>
+            <div class="w3-rest">
+                <input class="w3-input w3-border w3-round-large" name="txtDespesasExtra" type="text" placeholder="Despesas Extra" value="">
+            </div>
         </div>
-        <div class="w3-rest">
-            <input class="w3-input w3-border w3-round-large" name="txtDespesasExtra" type="text" placeholder="Despesas Extra" value="">
-        </div>
-    </div>
 
-    <div class="w3-row w3-section">
-        <div class="w3-col" style="width:11%;">
-            <i class="w3-xxlarge fab fa-angellist"></i>
+        <div class="w3-row w3-section">
+            <div class="w3-col" style="width:11%;" id="icones_dados_pessoais">
+                <i class="w3-xxlarge fab fa-angellist"></i>
+            </div>
+            <div class="w3-rest">
+                <input class="w3-input w3-border w3-round-large" name="txtTDRenda" type="text" placeholder="Tipo d/ renda" value="">
+            </div>
         </div>
-        <div class="w3-rest">
-            <input class="w3-input w3-border w3-round-large" name="txtTDRenda" type="text" placeholder="Tipo d/ renda" value="">
-        </div>
-    </div>
 
-    <div class="w3-row w3-section">
-        <div class="w3-col" style="width:11%;">
-            <i class="w3-xxlarge fas fa-glass-cheers"></i>
+        <div class="w3-row w3-section">
+            <div class="w3-col" style="width:11%;" id="icones_dados_pessoais">
+                <i class="w3-xxlarge fas fa-glass-cheers"></i>
+            </div>
+            <div class="w3-rest">
+                <input class="w3-input w3-border w3-round-large" name="txtTDDespesa" type="text" placeholder="Tipo D/ Despesas" value="">
+            </div>
         </div>
-        <div class="w3-rest">
-            <input class="w3-input w3-border w3-round-large" name="txtTDDespesa" type="text" placeholder="Tipo D/ Despesas" value="">
-        </div>
-    </div>
 
-    <div class="w3-row w3-section">
-        <div class="w3-col" style="width:11%;">
-            <i class="w3-xxlarge fab fa-avianex"></i>
+        <div class="w3-row w3-section">
+            <div class="w3-col" style="width:11%;" id="icones_dados_pessoais">
+                <i class="w3-xxlarge fab fa-avianex"></i>
+            </div>
+            <div class="w3-rest">
+                <input class="w3-input w3-border w3-round-large" name="txtTDDespesaExtra" type="text" placeholder="Tipo D/ Despesas Extra" value="">
+            </div>
         </div>
-        <div class="w3-rest">
-            <input class="w3-input w3-border w3-round-large" name="txtTDDespesaExtra" type="text" placeholder="Tipo D/ Despesas Extra" value="">
-        </div>
-    </div>
 
+    
     <div class="w3-center">
         <button name="btnAtualizar" class="w3-button w3-block w3-margin w3-black w3-cell w3-round-large" style="width: 80%;">Atualizar</button>
     </div>
 </div>
 
 <!-- Obetivo -->
-<div class="w3-padding-128 w3-content w3-text-grey" id="ifinanceira">
+<div class="w3-padding-128 w3-content w3-text-grey" id="robjetivo">
     <h2 class="w3-text-green">Objetivos</h2>
 
     <div class="w3-row w3-section">
-        <div class="w3-col" style="width:11%;">
+        <div class="w3-col" style="width:11%;" id="icones_dados_pessoais">
             <i class="w3-xxlarge fas fa-piggy-bank"></i>
         </div>
         <div class="w3-rest">
@@ -184,7 +188,7 @@
     </div>
 
     <div class="w3-row w3-section">
-        <div class="w3-col" style="width:11%;">
+        <div class="w3-col" style="width:11%;" id="icones_dados_pessoais">
             <i class="w3-xxlarge fas fa-calendar-alt"></i>
         </div>
         <div class="w3-rest">
@@ -193,7 +197,7 @@
     </div>
 
     <div class="w3-row w3-section">
-        <div class="w3-col" style="width:11%;">
+        <div class="w3-col" style="width:11%;" id="icones_dados_pessoais">
             <i class="w3-xxlarge fas fa-money-check-alt"></i>
         </div>
         <div class="w3-rest">
@@ -202,7 +206,7 @@
     </div>
 
     <div class="w3-row w3-section">
-        <div class="w3-col" style="width:11%;">
+        <div class="w3-col" style="width:11%;" id="icones_dados_pessoais">
             <i class="w3-xxlarge fas fa-hand-holding-usd"></i>
         </div>
         <div class="w3-rest">
