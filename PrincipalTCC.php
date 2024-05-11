6faab83cb507323883a6cc7b196152871acdf9ee
Ana Carolina <115048267+ana-caroll@users.noreply.github.com>
2024-05-11 17:25:05 -0300

    Update PrincipalTCC.php

--- PrincipalTCC.php
+++ PrincipalTCC.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="stylePrincipal.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <script src="https://kit.fontawesome.com/8a0118f486.js" crossorigin="anonymous"></script>
     <title>Controle Financeiro</title>
